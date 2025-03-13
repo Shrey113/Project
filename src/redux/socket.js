5f43403c01746432dab23d4e1c9f63b749fd8e3c
PraharshPatni13 <praharshpatni@gmail.com>
2025-03-13 12:09:09 +0530

    added socket logic

--- src/redux/socket.js
+++ src/redux/socket.js
@@ -1,13 +1,17 @@
 import { io } from 'socket.io-client';
 
-import { Server_url } from './AllData';
+
+import { Socket_url } from './AllData';
 
 
-const socket = io(Server_url, {
+const socket = io(Socket_url, {
+  origin: '*',
+  method: ['GET', 'POST', 'PUT', 'DELETE'],
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
 });
+ws://srv749838.hstgr.cloud:4000
 
 socket.on('connect_error', (error) => {
   console.error('Socket connection error:', error);
