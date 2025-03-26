cb0e13da1d1cd2f5b0c6f8f747617a1122a5adde
PraharshPatni13 <praharshpatni@gmail.com>
2025-03-26 14:25:56 +0530

    socket work trying

--- src/redux/socket.js
+++ src/redux/socket.js
@@ -5,13 +5,19 @@ import { Socket_url } from './AllData';
 
 
 const socket = io(Socket_url, {
-  origin: '*',
   method: ['GET', 'POST', 'PUT', 'DELETE'],
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
   transports: ["websocket", "polling"],
+  timeout: 5000,
 });
+console.log("socket url", socket);
+
+socket.on("connect", () => {
+  console.log("Conne  cted to WebSocket server");
+});
+
 
 socket.on('connect_error', (error) => {
   console.error('Socket connection error:', error);
